65a3ae61a58a27b70d82c958b70136037a2be62e
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2025-01-18 19:16:56 -0800

    More nonsense

--- hw10_riscv/build.sh
+++ hw10_riscv/build.sh
@@ -3,9 +3,8 @@ set -e
 
 clang -Os --target=riscv32 -march=rv32i -nostdlib -T link.ld -o firmware.elf entry.s main.c
 echo ====================================
-#llvm-objdump-18 -d firmware.elf
 riscv64-unknown-elf-objdump -d firmware.elf
 echo ====================================
-riscv64-unknown-elf-objcopy -O binary -j .text firmware.elf memory.bin
+riscv64-unknown-elf-objcopy -O binary firmware.elf memory.bin
 xxd memory.bin
 
